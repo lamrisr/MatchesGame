435362f2c8f643ab7a6b2cc884db56e1c45faae3
unknown <ridalamris@gmail.com>
2020-06-25 23:56:27 +0200

    First commit : Basic gameplay

---
+++ README.md
@@ -0,0 +1,18 @@
+# MatchesGame
+
+  This project uses Angular 7.2
+  Matches game consists in choosing each time a number of matches stick to pick (between 1 and 3),
+  the player to pick the last match stick loses the game.
+  Here you'll be playing against the computer.
+
+## How to start
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+
+export enum TURNS {
+  PLAYER,
+  COMPUTER
+}
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+
+  turn : TURNS = TURNS.PLAYER;
+  matchesCount : number;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.generateRandomMatchesNumber();
+  }
+
+  onChoiceDone(choice) {
+    this.matchesCount -= choice;
+    if(this.matchesCount > 0) this.giveHand();
+    if(this.turn === TURNS.COMPUTER) this.makeAChoice();
+  }
+
+  replay() {
+    this.turn = TURNS.PLAYER;
+    this.generateRandomMatchesNumber();
+  }
+
+  /**
+   * Sets matchesCount to a random value between 7 and 15
+   */
+  private generateRandomMatchesNumber() {
+    this.matchesCount = Math.floor(Math.random() * Math.floor(8)) + 7;
+  }
+
+  private giveHand() {
+    if(this.turn === TURNS.PLAYER) this.turn = TURNS.COMPUTER;
+    else if(this.turn === TURNS.COMPUTER) this.turn = TURNS.PLAYER;
+  }
+
+  private makeAChoice() {
+    let modulo = this.matchesCount % 4;
+    console.log("modulo "+modulo);
+    let matchesToPick = 0;
+    if(modulo === 0) matchesToPick = 3;
+    else if(modulo <= 3) matchesToPick = modulo;
+    console.log("qdqd "+matchesToPick);
+    this.matchesCount -= matchesToPick;
+    if(this.matchesCount > 0) this.giveHand();
+  }
+
+}
